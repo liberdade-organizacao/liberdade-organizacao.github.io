61e2d2658ece4d0f483ae4efbcedcdfeccec5ed7
Cristiano Silva Júnior <ishiikurisu@yahoo.com>
2019-05-30 10:18:54 -0300

    Using github-blog tool to load posts

--- js/blog.js
+++ js/blog.js
@@ -10,16 +10,15 @@ function draw(posts) {
 
     for (var i = 0; i < posts.length; i++) {
         var post = posts[i];
-        var url = `/post.html?link=`+encodeURI(post.link)+`&title=`+encodeURI(post.title);
-        // TODO implement post page
+        var url = `/post.html?link=${encodeURI(post.link)}&title=${encodeURI(post.title)}`;
         outlet += `
             <div class="l-box">
                 <h3 class="information-head">
-                    <a href="`+url+`">
-                        `+post.title+`
+                    <a href="${url}">
+                        ${post.title}
                     </a>
                 </h3>
-                `+post.description+`
+                ${post.description}
             </div>
             <hr>
         `;
@@ -28,31 +27,7 @@ function draw(posts) {
     document.getElementById('posts').innerHTML = outlet;
 }
 
-function get(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            callback(data);
-        } else {
-            callback({error: "Cuma?"});
-        }
-    };
-
-    request.onerror = function() {
-        // There was a connection error of some sort
-        callback({error: "Não conseguimos carregar os posts :("});
-    };
-
-    request.send();
-}
-
 function main() {
-    // TODO Implement pagination logic
-    // TODO Include descriptions to each post
-    var url = "https://www.gitcdn.xyz/repo/liberdade-organizacao/posts/master/index.json";
-
-    get(url, draw);
+    var blog = new GithubBlog('liberdade-organizacao/posts');
+    blog.loadIndex(draw);
 }
